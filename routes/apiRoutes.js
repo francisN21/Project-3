2777c50ba35276d7f7fe8628875dc7d756452bfe
Brandon Tabaska <btabaska@gmail.com>
2021-03-06 13:02:01 -0800

    Added Models and Basic Routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,40 @@
 // const axios = require("axios")
-const router = require("express").Router()
-// const db = require("../models")
+const router = require("express").Router();
+const db = require("./models");
 
 router.get("/test", (req, res) => {
-    res.send({ msg: "success" });
+  res.send({ msg: "success" });
 });
 
-module.exports = router
+// Route for creating a new User
+router.post("/user", function (req, res) {
+  db.User.create(req.body)
+    .then(function (dbUser) {
+      // If we were able to successfully create a User, send it back to the client
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+//Route to Create a new Saved Location.
+router.post("/location/:id", function (req, res) {
+  db.Saved.create(req.body)
+    .then(function (dbSaved) {
+      return db.User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { saved: dbSaved._id } }
+      );
+    })
+    .then(function (dbSaved) {
+      res.json(dbSaved);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+module.exports = router;
