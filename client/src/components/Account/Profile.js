3e2e9fb6f9e8a72913e972e0e6cbb6a25361bade
Sean Melody <seandmelody@gmail.com>
2021-03-09 20:31:04 -0800

    Users rconsole logging to profile page

--- client/src/components/Account/Profile.js
+++ client/src/components/Account/Profile.js
@@ -6,6 +6,31 @@ const Profile = () => {
 
 
 
+  const getUsers = () => {
+    fetch('/api/user', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Json that response
+    })
+      .then((response) => response.json())
+      // Then get the data
+      .then((data) => {
+        // Console log the data 
+        console.log(data)
+
+
+
+      })
+
+  }
+  // use Effect to call the get users function
+  useEffect(() => {
+    getUsers()
+    // eslint-disable-line no-alert
+  }, [])
+
   return <div>Hello From Profile</div>;
 };
 
