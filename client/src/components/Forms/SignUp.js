6b44f3e6b1df2eb2e1c4029fd80d088f98ddad08
Edgar Pena <edgarignaciopena@gmail,com>
2021-03-11 16:20:54 -0800

    added submit function to SignUp.js// trying to figure out 404 error

--- client/src/components/Forms/SignUp.js
+++ client/src/components/Forms/SignUp.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const SignUp = () => {
   //use onSubmit
-  const saveUser = (e) => {
-    console.log(e.target.value);
+  const [signUpForm, setSignUpForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e) => {
+    setSignUpForm({ ...setSignUpForm, [e.target.name]: e.target.value });
+  };
+
+  const submit = async (e) => {
+    e.preventDefault(e);
+    try {
+      await axios.post("/api/user", signUpForm);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="signup-form">
       <h2>Sign Up</h2>
-      <form>
+      <form onSubmit={submit}>
         <div className="form-group">
           <label>Name</label>
           <br></br>
           <input
             type="text"
+            name="firstName"
             className="form-control"
             id="name-input"
-            placeholder="Full Name"
+            placeholder="First Name"
+            onChange={onChange}
+          ></input>
+          <input
+            type="text"
+            name="lastName"
+            className="form-control"
+            id="name-input"
+            placeholder="Last Name"
+            onChange={onChange}
           ></input>
         </div>
         <div className="form-group">
@@ -26,9 +52,11 @@ const SignUp = () => {
           <br></br>
           <input
             type="email"
+            name="email"
             className="form-control"
             id="email-input"
             placeholder="Email"
+            onChange={onChange}
           ></input>
         </div>
         <div className="form-group">
@@ -36,12 +64,14 @@ const SignUp = () => {
           <br></br>
           <input
             type="password"
+            name="password"
             className="form-control"
             id="password-input"
             placeholder="Password"
+            onChange={onChange}
           ></input>
         </div>
-        <button type="submit" className="btn btn-primary" onSubmit={saveUser}>
+        <button type="submit" className="btn btn-primary">
           Create account
         </button>
       </form>
