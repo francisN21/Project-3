906e1d5a70297f5fc0d40ee8d2d48c4797f00be5
Sean Melody <seandmelody@gmail.com>
2021-03-11 21:13:46 -0800

    Portfolio Page dispalying first user

--- client/src/components/Account/Profile.js
+++ client/src/components/Account/Profile.js
@@ -1,37 +1,96 @@
-import React, { useEffect } from "react";
+// import the react goodness!
+import React, { useEffect, useState } from "react";
 
 const Profile = () => {
 
+  // Use State for the users
+  const [users, setUsers] = useState([
+    {
+      firstName: "",
+      lastName: "",
+      username: "",
+      saved: [],
+      friends: [],
+    }])
 
 
-
-
+  // Function to get the the user from the database
+  // Will need to change to ID so we don't get all users
   const getUsers = () => {
     fetch('/api/user', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
-      // Json that response
     })
       .then((response) => response.json())
       // Then get the data
       .then((data) => {
         // Console log the data 
         console.log(data)
-
-
-
+        //Set the data to users!
+        setUsers(data)
       })
 
   }
-  // use Effect to call the get users function
+  // use Effect to call the get users function when the page loads
   useEffect(() => {
     getUsers()
     // eslint-disable-line no-alert
   }, [])
 
-  return <div>Hello From Profile</div>;
+  // console.log(users)
+
+  // Styling!
+  // Basic Card Styling
+  const cardStyles = {
+    margin: "20px"
+  }
+  // Basic List Styling
+  const listStyles = {
+    listStyle: "none",
+  }
+
+  // Return it all!
+  return (
+    <div className="container">
+      {/* If there is a user, display it */}
+      {users.length ? (
+        <div className="card" style={cardStyles}>
+          {/* Card title for name */}
+          <h1 className="card-title text-center">{users[0].firstName} {users[0].lastName}</h1>
+          {/* Break */}
+          <hr></hr>
+          <div className="card-body">
+            {/* Display username, email */}
+            <h4>Username: {users[0].username}</h4>
+            <h5>Email: {users[0].email}</h5>
+            {/* Iterate through the Saved items for the user */}
+            <ul style={listStyles}>
+              <h4>Saved:</h4>
+              {users[0].saved.map(item => {
+                <li>{item}</li>
+              })
+              }
+            </ul>
+            {/* Iterate through the Friends of the user */}
+            <ul style={listStyles}>
+              <h4>Friends:</h4>
+              {users[0].friends.map(item => {
+                <li>{item}</li>
+              })
+              }
+            </ul>
+          </div>
+        </div>
+      ) : (
+          // If no users, display this
+          <h1>No users yet</h1>
+        )
+      }
+    </div >
+  )
 };
 
+//Export it all
 export default Profile;
