4e48eee29a356f207e656aebc51b554058acacf2
Sean Melody <seandmelody@gmail.com>
2021-03-12 13:50:58 -0800

    cards added to Dashboard

--- client/src/components/Account/Dashboard.js
+++ client/src/components/Account/Dashboard.js
@@ -1,7 +1,38 @@
 import React from "react";
 
+
+
+const cardStyles = {
+  margin: "20px",
+  width: "18rem"
+}
 const Dashboard = () => {
-  return <div>Hello From Dashboard</div>;
+  return (
+    <>
+      <h1>Your Events</h1>
+      <div className="container">
+        <div className="card" style={cardStyles}>
+          <h1 className="card-title text-center">Event.title</h1>
+          <hr></hr>
+          <div className="card-body">
+            <h5>Event.latitude</h5>
+            <h5>Event.longitude</h5>
+            <h5>Event.date</h5>
+          </div>
+
+
+          <div className="card-footer">
+            <button
+              className="btn btn-danger"
+              onClick={() => console.log("Delete")}
+            >
+              Delete Profile
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default Dashboard;
