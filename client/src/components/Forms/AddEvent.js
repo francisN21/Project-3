b873458dd8dc35db39843ed1468bd24a22959369
Sean Melody <seandmelody@gmail.com>
2021-03-13 12:30:27 -0800

    comments added to code

--- client/src/components/Forms/AddEvent.js
+++ client/src/components/Forms/AddEvent.js
@@ -1,21 +1,27 @@
+// Import all the react goodness!
 import React, { useState } from 'react'
 
+// Add Event Page component
 const AddEvent = () => {
 
+    // State for adding an event to the Log Event database
     const [newEvent, setNewEvent] = useState();
 
+    // On change to set the value of the form to state
     const onChange = (e) => {
         setNewEvent({ ...newEvent, [e.target.name]: e.target.value });
     };
 
-    // // When the form is submitted, search the API for the value of `this.state.search`
+    // On Submit to prevent default behavior of the form
     const onSubmit = (e) => {
         // Gotta prevent the default!
         e.preventDefault()
 
     };
+
+    // Save Event function to save an event to the LogEvent database
     const saveEvent = (newEvent) => {
-        console.log(newEvent)
+        // Set the event to an object and parse int the numbers
         newEvent = {
             title: newEvent.title,
             description: newEvent.description,
@@ -25,6 +31,7 @@ const AddEvent = () => {
             timestamps: true,
         }
         console.log(newEvent)
+
         // Send fetch request to post it to the database
         fetch(`/api/events`, {
             method: 'POST',
@@ -34,82 +41,73 @@ const AddEvent = () => {
             // json that response and let the user know that it was saved
             .then((response) => response.json())
             .then((data) => {
-
                 console.log(`${newEvent.title} saved`)
             })
     }
 
+    // Return the component
     return (
+        // Div to center the content
         <div className="container text-center justify-content-center">
-            <h1>Hi from the Add Event Page</h1>
+            {/* Title of page */}
+            <h1>Add An Event</h1>
+            {/* Set up the form and prevent defualt behavior*/}
             <form className="border-dark"
                 onSubmit={onSubmit}
             >
-                {/* Header Component here */}
-
                 <div className="form-group">
                     <div className="col-md-9">
-                        {/* Input form for searching the GoogleBooksAPI */}
+                        {/* Input forms to add the event */}
+                        {/* Input for title */}
                         <input
                             onChange={onChange}
-                            // value={props.search}
                             name="title"
                             type="text"
                             placeholder="Event Title"
                             className="form-control text-center border border-dark"
-                        // id="search"
                         />
+                        {/* Input for description */}
                         <input
                             onChange={onChange}
-                            // value={props.search}
                             name="description"
                             type="text"
                             placeholder="description"
                             className="form-control text-center border border-dark"
-                        // id="search"
                         />
-
+                        {/* Input for latitude */}
                         <input
                             onChange={onChange}
-                            // value={props.search}
                             name="latitude"
                             type="number"
                             step="0.00001"
                             placeholder="latitude"
                             className="form-control text-center border border-dark"
-                        // id="search"
                         />
+                        {/* Input for longitude */}
                         <input
-                            // onChange={props.handleInputChange}
                             onChange={onChange}
-                            // value={props.search}
                             name="longitude"
                             type="number"
                             step="0.00001"
                             placeholder="longitude"
                             className="form-control text-center border border-dark"
-                        // id="search"
                         />
+                        {/* Input for date */}
                         <input
-                            // onChange={props.handleInputChange}
-                            // value={props.search}
                             onChange={onChange}
                             name="date"
                             type="date"
                             placeholder="date of event"
                             className="form-control text-center border border-dark"
-                        // id="search"
                         />
 
-                        {/* Button to start the search */}
+                        {/* Button to save the event */}
                         <button
                             onClick={() => saveEvent(newEvent)}
-                            className="btn btn-outline-primary">
-                            Add Event
+                            className="btn btn-primary">
+                            Save Event
                          </button>
                     </div>
-
-
                 </div>
             </form>
         </div>
