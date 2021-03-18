4a7f421400946111f6d8b2ad4cfadf225ff7407b
Sean Melody <seandmelody@gmail.com>
2021-03-17 19:33:20 -0700

    Clusters Page console logging database Events

--- client/src/components/Clusters/Clusters.js
+++ client/src/components/Clusters/Clusters.js
@@ -1,10 +1,12 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { render } from 'react-dom';
 import ReactMapGL, { Source, Layer } from 'react-map-gl';
 import { clusterLayer, clusterCountLayer, unclusteredPointLayer } from './ClusterLayers';
+import { listEvents } from "../../utils/API";
+
 
 const Clusters = () => {
-
+    const [showevents, setEvents] = useState([]);
     const [viewport, setViewport] = useState({
         latitude: 40.67,
         longitude: -103.59,
@@ -12,11 +14,25 @@ const Clusters = () => {
         height: "100vh",
         zoom: 3,
     });
-
+    const mapRef = useRef(null);
 
     const MAPBOX_TOKEN = `pk.eyJ1IjoiZnJhbmNpc24yMSIsImEiOiJja2x1amVuNGQwYmVkMm9vZW9xc3VwOW9jIn0.eh8hBFzSr0tJUxungpfu3A`
     const map_style = "mapbox://styles/mapbox/dark-v9"
 
+
+    const getEvents = async () => {
+        try {
+            const showMarkers = await listEvents();
+            console.log(showMarkers)
+            setEvents(showMarkers);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getEvents();
+    }, []);
 
 
     return (
