3ed39f8b39d8c374e9219b2ba9a697d59e74bdc9
Sean Melody <seandmelody@gmail.com>
2021-03-17 19:27:53 -0700

    clusters page added and displaying a basic map

---
+++ client/src/components/Clusters/Clusters.js
@@ -0,0 +1,45 @@
+import React, { useState, useRef } from 'react'
+import { render } from 'react-dom';
+import ReactMapGL, { Source, Layer } from 'react-map-gl';
+import { clusterLayer, clusterCountLayer, unclusteredPointLayer } from './ClusterLayers';
+
+const Clusters = () => {
+
+    const [viewport, setViewport] = useState({
+        latitude: 40.67,
+        longitude: -103.59,
+        width: "100vw",
+        height: "100vh",
+        zoom: 3,
+    });
+
+
+    const MAPBOX_TOKEN = `pk.eyJ1IjoiZnJhbmNpc24yMSIsImEiOiJja2x1amVuNGQwYmVkMm9vZW9xc3VwOW9jIn0.eh8hBFzSr0tJUxungpfu3A`
+    const map_style = "mapbox://styles/mapbox/dark-v9"
+
+
+
+    return (
+        <ReactMapGL
+            {...viewport}
+            maxZoom={20}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+            onViewportChange={setViewport}
+            mapStyle={map_style}
+        >
+
+
+
+        </ReactMapGL>
+    )
+}
+
+export default Clusters
+
+
+
+
+
+
+
+
