a12032b2437ecc016c896715a14cd42549acc37c
Sean Melody <seandmelody@gmail.com>
2021-03-23 18:14:18 -0700

    heroku

--- client/src/components/Map/EntryForm.js
+++ client/src/components/Map/EntryForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
-import { createEvent } from "../../utils/API";
+import { createEvent } from "../../utils/API"
 
 const EntryForm = ({ location, onClose }) => {
   const [eventForm, setEvent] = useState({
